2444d440dd81566c4381fa60fa9c8eedfc1c22eb
BuildTools <unconfigured@null.spigotmc.org>
2016-02-07 14:36:27 -0500

    added start function for autonomous record

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EndGameBlueRecord.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EndGameBlueRecord.java
@@ -11,6 +11,7 @@ public class EndGameBlueRecord extends LinearOpMode {
     FTCRobot robot;
     String writeFilePath = "/sdcard/FIRST/autonomousCmds/endGameBlue.csv";
     String readFilePath = null;
+    boolean startRecording = false;
 
     @Override
     public void runOpMode() throws InterruptedException{
@@ -20,6 +21,12 @@ public class EndGameBlueRecord extends LinearOpMode {
 
         waitForStart();
 
-        robot.runRobotTeleop();
+        if(gamepad1.start){
+            startRecording = true;
+        }
+        if (startRecording) {
+            robot.timestamp = System.nanoTime();
+            robot.runRobotTeleop();
+        }
     }
 }
